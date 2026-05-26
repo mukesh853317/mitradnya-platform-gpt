fd2349381620c24297868376877f00f7133091ec
mukesh853317 <mukeshamrutkar.shm@gmail.com>
2026-05-26 22:51:10 +0530

    Update table_renderer.py

--- utils/table_renderer.py
+++ utils/table_renderer.py
@@ -1,2 +1,56 @@
-if "|" in line:
-    table_data.append([c.strip() for c in line.split("|")])
+import streamlit as st
+
+def render_html_table(data):
+
+    if not data:
+        return
+
+    html = """
+    <table style="
+        width:100%;
+        border-collapse:collapse;
+        margin-bottom:20px;
+        background:white;
+        color:black;
+        font-size:15px;
+    ">
+    """
+
+    for r_idx, row in enumerate(data):
+
+        html += "<tr>"
+
+        for col in row:
+
+            if r_idx == 0:
+
+                html += f"""
+                <th style="
+                    border:1px solid #ccc;
+                    padding:10px;
+                    background:#1f2937;
+                    color:white;
+                    text-align:center;
+                    font-weight:bold;
+                ">
+                    {col}
+                </th>
+                """
+
+            else:
+
+                html += f"""
+                <td style="
+                    border:1px solid #ccc;
+                    padding:10px;
+                    text-align:left;
+                ">
+                    {col}
+                </td>
+                """
+
+        html += "</tr>"
+
+    html += "</table>"
+
+    st.markdown(html, unsafe_allow_html=True)
