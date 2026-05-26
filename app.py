0de46ef570ae5123c4cab93ac7c457b488995427
mukesh853317 <mukeshamrutkar.shm@gmail.com>
2026-05-26 22:41:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import streamlit as st
 from pages.student_dashboard import show_student_dashboard
 from pages.admin_dashboard import show_admin_dashboard
 
+# ====================================================
+# PAGE CONFIG
+# ====================================================
+
 st.set_page_config(
     page_title="Mitradnya Learning Platform",
     layout="wide"
@@ -10,13 +14,25 @@ st.set_page_config(
 
 st.title("📚 Mitradnya Learning Platform")
 
+# ====================================================
+# MAIN TABS
+# ====================================================
+
 main_tabs = st.tabs([
     "🎓 Student Dashboard",
     "👨‍🏫 Admin Dashboard"
 ])
 
+# ====================================================
+# STUDENT DASHBOARD
+# ====================================================
+
 with main_tabs[0]:
     show_student_dashboard()
+
+# ====================================================
+# ADMIN DASHBOARD
+# ====================================================
 
 with main_tabs[1]:
     show_admin_dashboard()
